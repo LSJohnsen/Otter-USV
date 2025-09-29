85132f4b14a8ad1ed17d7090fc3888362ccae2c3
Lars Skogen Johnsen <Lars.skogenjohnsen@gmail.com>
2025-09-29 15:45:45 +0200

    mpc control start

--- Otter_API/casadi_control/MPC_control.py
+++ Otter_API/casadi_control/MPC_control.py
@@ -1,18 +1,46 @@
 
 
 import casadi as ca
-
+from model_3dof import Otter3DOF 
 
 class NMPCControl:
-    def __init__(self, model, N):
+    def __init__(self, usv_model, N, sampleTime=0.02, solver="rk"):
+
+
+        # USV states
+        self.usv_model = usv_model
+        self.x = self.usv_model.x
+        self.u = self.usv_model.u
+        self.ode = self.usv_model.ode
+
+        # Target states
+        self.current_target = None
+        self.previous_target = None
+        self.target = None
+
+        # MPC options
+        self.N = N
+
+
+
 
 
         
         self.N = N
         opti = ca.Opti()
 
-        x = opti.variable(6,N+1)
-        u = opti.vaiable(3,N+1) 
+        x = opti.variable(6,N+1) # States
+        u = opti.variable(3,N+1) # Controls
+        p = opti.parameter(6) 
+        target = opti.parameter(2) # target state
+
+
+
+
+        # Cost function parameters - ENDRE 
+        Q = opti.parameter(2, 2) # Control variable weights (output (CV, y))
+        R = opti.parameter(3, 3) # Manipulated variable weights (Input (MU, u)
+        I = opti.parameter(3, 3)
 
     
     def control_step(f, sampleTime):
@@ -25,7 +53,7 @@ class NMPCControl:
         dae = {
         'x': x,                       # states (eta, nu)
         'p': ca.vertcat(u, p),        # parameters (inputs + currents) CHANGE TO ONLY CONTROLS OR ADD FILTER/ESTIMATION
-        'ode': f(x, u, p)            # dynamics (x, u, p): see 3DOF_model.py
+        'ode': f(x, u, p)            # dynamics (x, u, p) -> f inherits the ODE: see 3DOF_model.py
         }    
         
         # Integrator
--- Otter_API/casadi_control/model_3dof.py
+++ Otter_API/casadi_control/model_3dof.py
@@ -14,26 +14,22 @@ Otter USV 3-DOF CasADi model
 
 params = usv_params_6dof()
 
-class Otter3DOF:
-    def __init__(self, params, sampleTime):
-
-
-        self.reduced = ca.DM([[1,0,0,0,0,0], # Selects 3DOF parameters from sim 6DOF matrix 
+def Otter3DOF(params):
+        reduced = ca.DM([[1,0,0,0,0,0], # Selects 3DOF parameters from sim 6DOF matrix 
                         [0,1,0,0,0,0],         # reduced @ X_6DOF @ reduced.T = X_3DOF 
                         [0,0,0,0,0,1]])        # rows 1,2,6 @ X @ pick colums 1,2,6 
 
 
         # 6DOF mass & damping to 3DOF
-        self.M3 = self.reduced @ (params['MRB6'] + params['MA6']) @ self.reduced.T ### Change matrix operations to ca.mtimes(m1, m2, m3) if i mix dm/sx etc.
-        self.D3 = self.reduced @ params['D6'] @ self.reduced.T
+        M3 = reduced @ (params['MRB6'] + params['MA6']) @ reduced.T ### Change matrix operations to ca.mtimes(m1, m2, m3) if i mix dm/sx etc.
+        D3 = reduced @ params['D6'] @ reduced.T
 
 
         # Constants from sim
-        self.Ig = params['Ig'] # inertia around CG
-        self.H_rg = params['H_rg'] # CG to CO transfrom 
-        self.m_total = params['m_total'] # mass
-        self.MA3 = self.reduced @ params['MA6'] @ self.reduced.T # 
-        self.dt = sampleTime # Integration step
+        Ig = params['Ig'] # inertia around CG
+        H_rg = params['H_rg'] # CG to CO transfrom 
+        m_total = params['m_total'] # mass
+        MA3 = reduced @ params['MA6'] @ reduced.T # 
 
         # ---
         eta = ca.SX.sym('eta', 3) # [x, y, psi]
@@ -41,23 +37,23 @@ class Otter3DOF:
         u =  ca.SX.sym('u', 3) # [u1, u2] 
         nu_c = ca.SX.sym('nu_c', 3) # Currents in body
         x = ca.vertcat(eta, nu) # [x,y,psi,u,v,r]
-        p    = nu_c
+        p = nu_c
 
         J = B2N(eta[2]) #Body to NED transfrom 
 
         # Rigid-Body Coriolis to 3DOF 
         nu6  = ca.vertcat(nu[0],nu[1],0,0,0,nu[2])
-        CRB6 = CRB6sx(nu6, self.m_total, self.Ig, self.H_rg)
-        CRB3 = self.reduced @ CRB6 @ self.reduced.T 
+        CRB6 = CRB6sx(nu6, m_total, Ig, H_rg)
+        CRB3 = reduced @ CRB6 @ reduced.T 
 
         nu_r = nu - nu_c # Relative velocity
         
-        CA3 = CA3sx(self.MA3, nu_r)
+        CA3 = CA3sx(MA3, nu_r)
         C3 = CRB3 + CA3
 
         # Hydrodynamic linear damping + nonlinear yaw damping (from simulator)
-        tau_d = self.D3 @ nu_r
-        tau_d[2] += 10 * self.D3[2,2] * ca.fabs(nu_r[2]) * nu_r[2]
+        tau_d = D3 @ nu_r
+        tau_d[2] += 10 * D3[2,2] * ca.fabs(nu_r[2]) * nu_r[2]
 
         # cross-flow drag 
         tau_cfd = crossFlowDrag3(params['L'], params['B'], params['T'], nu_r) 
@@ -73,22 +69,9 @@ class Otter3DOF:
         '''
 
         ode = ca.vertcat(J @ nu,                    # compute the RHS forces from fossens eq (ODE for 3DOF USV)              
-                        ca.solve(self.M3,           
+                        ca.solve(M3,           
                         u - C3 @ nu - tau_d - tau_cfd)  
                         ) 
-        
-        # DAE
-        dae = {
-        'x': x,                         # states (eta, nu)
-        'p': ca.vertcat(u, p),        # parameters (inputs + currents) CHANGE TO ONLY CONTROLS OR ADD FILTER/ESTIMATION
-        'ode': ode                      # dynamics (x, u, p)
-        }    
-        
-        # Integrator
-        integrator = ca.integrator(
-        'integrator', 'rk', dae,
-        {'tf': self.dt, 'simplify': True, 'number_of_finite_elements': 4}
-        )
      
 
         # continous time function ([state vector, control input, nu_c], rhs_function)
